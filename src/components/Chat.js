0799596d463658b093615b26017e4ee6ac9d00c8
carlon522 <carlogulliwork@gmail.com>
2024-07-28 03:13:20 +0200

    hotfix pre encryption

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import MessageList from './MessageList';
 import SendMessage from './SendMessage';
-import { Container, Box, Typography } from '@mui/material';
+import { Container, Box, Button } from '@mui/material';
+import { collection, getDocs, deleteDoc, doc } from 'firebase/firestore';
+import { db } from '../auth';
 
 const Chat = () => {
+  const handleClearChat = async () => {
+    const querySnapshot = await getDocs(collection(db, "messages"));
+    const batch = db.batch(); // using batch for bulk operations
+
+    querySnapshot.forEach(docSnapshot => {
+      const messageRef = doc(db, "messages", docSnapshot.id);
+      batch.delete(messageRef);
+    });
+
+    await batch.commit().then(() => {
+      console.log("Chat cleared!");
+    });
+  };
+
   return (
     <Container
       maxWidth="md"
-      style={{ display: 'flex', flexDirection: 'column', height: 'calc(100vh - 64px)' }} 
+      style={{ display: 'flex', flexDirection: 'column', height: 'calc(100vh - 64px)' }} // Adjust height to avoid overlapping with the AppBar
     >
-      <Typography variant="h4" style={{ textAlign: 'center', margin: '20px 0' }}>
-        Chat
-      </Typography>
+      <Box
+        display="flex"
+        justifyContent="flex-end"
+        mb={1}
+      >
+        <Button variant="contained" color="secondary" onClick={handleClearChat}>
+          Clear Chat
+        </Button>
+      </Box>
       <Box
         style={{
           flex: 1,
